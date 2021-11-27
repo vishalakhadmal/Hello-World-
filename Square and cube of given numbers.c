acf8343f46335e7308ce7a48b41988615abb3ca0
vishalakhadmal <95120989+vishalakhadmal@users.noreply.github.com>
2021-11-27 13:34:47 +0530

    Create Square and cube of given numbers.c

---
+++ Square and cube of given numbers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int num;
+    printf("\n\n For Calculating Square and ccube of given number");
+    printf("\n\n Enter any number = ");
+    scanf("%d",&num);
+    printf("\n\n Square of %d = %d", num, num * num );
+    printf("\n\n Cube of %d = %d",num, num * num * num );
+}
+
